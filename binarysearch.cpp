63eb7b5abde351677bc23576c6f3081e857fd092
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2022-01-03 12:16:19 +0800

    Add files via upload

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 #include"mergesort.cpp"
 using namespace std;
+/*binary search's idea is:
+when have an array of numbers say 5,3,6,1
+first sort the array(here I use mergesort whose running time is nlogn) 
+since we need the property of sorted array, now the array is 1,3,5,6
+Then we compare the target number t with the median m
+if t >= m , we now do the binary search on the number including and after m (ie. 5,6)
+otherwise the number before m(ie. 1,3)
+*/ 
 bool binarysearch(int A[],int p,int q,int num){
     if(p==q&&A[p]==num){return true;}
     else if(p==q&&A[p]!=num){return false;}
@@ -8,6 +16,8 @@ bool binarysearch(int A[],int p,int q,int num){
     if(A[r]>=num){return binarysearch(A,p,r,num);}
     if(A[r]<num){return binarysearch(A,r+1,q,num);}
 };
+
+
 int main(){
     int size=0;
     int* input;
@@ -29,7 +39,8 @@ int main(){
                    }
         else{input= new int[1];
             input[0]=number;}
-    }// handling input
+    }
+    /////////////////////////// handling input///////////////////////////////
     
     for(int i=0;i<size;i++){
         cout<<input[i]<<"\t";
@@ -38,7 +49,7 @@ int main(){
     cout<<"what's the number that you want?";
     int wantednum;
     cin>>wantednum;
-    mergesort(input,0,size-1);
+    mergesort(input,0,size-1);// sort the array 
     if(binarysearch(input,0,size-1,wantednum)){cout<<"the number is found";}
     else{cout<< "number is not found";}
 }
--- mergesort.cpp
+++ mergesort.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
 using namespace std;
+//running time:
+// the running time of the merge sort is nlogn
+
+// intuition:
+//mergesort has two processes: mergesort the subproblem and merge
+//mergesort the subproblem will first sort the subarrays and merge will 
+// merge the subarrays into a sorted whole array.
+
+//detail:
+//mergesort the subproblem:
+//we divide the problem into smaller pieces, and recursively call the mergesort
+//the base case is when the subproblem only has one element
+//merge:
+//we create two arrays L array and R array to contain the elements in the left and right subproblems
+//at this point, the subproblems are already sorted, now imagine there are two pointers l and r pointing at the start
+//of the L and R array accordingly,and insert a infinite large number at the end of each array.
+//just to illustrate, we create another array F to contain the final result, but actually we just put the 
+// sorted elements back to the original array 
+//now we compare the numbers the l and r are pointing at
+//if the the l number is smaller than r number, we put the l number into the first place of F
+//and move the l pointer to the next element of L
+//vice versa
+//continue this process until there's no more element to compare
+
+
 void merge(int A[],int p,int r,int q){
     int L[r-p+2];
     int R[q-r+1];//q-(r+1)+1+1
